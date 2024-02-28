f2beacccd69178d5933b37fa396ccf28a4fd9b35
Erwin Nahue <erwinnahue@gmail.com>
2024-02-27 23:35:06 -0500

    Added error logging to TiendaContextSeed class

--- Infrastructure/Data/TiendaContext.cs
+++ Infrastructure/Data/TiendaContext.cs
@@ -1,18 +1,22 @@
-
-
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Data
-{
-    public class TiendaContext: DbContext
-    {
-        public TiendaContext(DbContextOptions<TiendaContext> options): base (options){
+namespace Infrastructure.Data;
 
-        }
-        public DbSet<Producto> Productos {get; set;}
-        public DbSet<Marca> Marcas {get; set;}
-        public DbSet<Categoria> Categorias {get; set;}
+public class TiendaContext : DbContext
+{
+    public TiendaContext(DbContextOptions<TiendaContext> options) : base(options)
+    {
 
     }
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Marca> Marcas { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
 }
