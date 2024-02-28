afce6acc8349b418e6e3ad096377425f6c1adcff
Erwin Nahue <erwinnahue@gmail.com>
2024-02-27 23:36:33 -0500

    Added error logging to TiendaContextSeed class

--- Infrastructure/Data/TiendaContextSeed.cs
+++ Infrastructure/Data/TiendaContextSeed.cs
@@ -1,12 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
-namespace Infrastructure.Data
-{
-    public class TiendaContextSeeds
+namespace Infrastructure.Data;
+
+    public class TiendaContextSeed
     {
-        
+        public static async Task SeedAsync(TiendaContext context, ILoggerFactory loggerFactory){
+            try{
+                
+
+            }
+            catch(Exception ex){
+                var logger = loggerFactory.CreateLogger<TiendaContextSeed>();
+                logger.LogError(ex.Message);
+            }
+        }
     }
-}
