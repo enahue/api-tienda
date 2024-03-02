a7e702e707b9181185894e684730d1c1ef8fe78e
Erwin Nahue <erwinnahue@gmail.com>
2024-03-01 23:02:24 -0500

    Implemented data seeding for TiendaContext with CSV files

--- Infrastructure/Data/TiendaContextSeed.cs
+++ Infrastructure/Data/TiendaContextSeed.cs
@@ -1,59 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Core.Entities;
+using CsvHelper;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data;
 
-    public class TiendaContextSeed
-    {
-        public static async Task SeedAsync(TiendaContext context, ILoggerFactory loggerFactory){
-            try{
-                var ruta = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                if(!context.Marca.Any()){
-                    using(var readerMarcas = new StreamReader(ruta + @"/DATA/Csvs/marcas.csv")){
-                            using(var csvMarcas = new CsvReader(readerMarcas, cultureInfo.InvariantCulture)){
-                                var marcas = csvMarcas.GetRecords<Marca>();
-                                context.Marca.AddRange(marcas);
-                                await context.SaveChangesAsync();
-                            }
-                    }
-                }
-                if(!context.Categorias.Any()){
-                    using(var readerCategorias = new StreamReader(ruta + @"/DATA/Csvs/categorias.csv")){
-                            using(var csvCategorias = new CsvReader(readerCategorias, cultureInfo.InvariantCulture)){
-                                var categorias = csvCategorias.GetRecords<Categoria>();
-                                context.Categoria.AddRange(categorias);
-                                await context.SaveChangesAsync();
-                            }
-                    }
-                }
-                if(!context.Productos.Any()){
-                    using(var readerProductos = new StreamReader(ruta + @"/DATA/Csvs/productos.csv")){
-                            using(var csvCategorias = new CsvReader(readerCategorias, cultureInfo.InvariantCulture)){
-                                var listadoProductosCsv = csvProductos.GetRecords<Producto>();
-                                List<Producto> productos = new List<Producto>();
-                                foreach (var item in listadoProductosCsv)
-                                {
-                                    productos.Add(new Producto(
-                                        Id = item.Id,
-                                        Nombre = item.Nombre,
-                                        Precio = item.Precio,
-                                        FechaCreacion = item.FechaCreacion,
-                                        CategoriaId = item.CategoriaId,
-                                        MarcaId = item.MarcaId
-                                    ));
-                                }
-                                context.Productos.AddRange(productos);
-                                await context.SaveChangesAsync();
+public class TiendaContextSeed
+{
+    public static async Task SeedAsync(TiendaContext context, ILoggerFactory loggerFactory){
+        try
+        {
+            var ruta = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-                            }
+            if(!context.Marcas.Any()){
+                using(var readerMarcas = new StreamReader(ruta+@"/Data/Csvs/marcas.csv")){
+                    using(var csvMarcas = new CsvReader(readerMarcas,CultureInfo.InvariantCulture)){
+                        var marcas = csvMarcas.GetRecords<Marca>();
+                        context.Marcas.AddRange(marcas);
+                        await context.SaveChangesAsync();
                     }
                 }
-
             }
+
+            if(!context.Categorias.Any()){
+                using(var readerCategorias = new StreamReader(ruta+@"/Data/Csvs/categorias.csv")){
+                    using(var csvCategorias = new CsvReader(readerCategorias,CultureInfo.InvariantCulture)){
+                        var categorias = csvCategorias.GetRecords<Categoria>();
+                        context.Categorias.AddRange(categorias);
+                        await context.SaveChangesAsync();
+                    }
+                }
+            }
+
+
+            if(!context.Productos.Any()){
+                using(var readerProductos = new StreamReader(ruta+@"/Data/Csvs/productos.csv")){
+                    using(var csvProductos = new CsvReader(readerProductos,CultureInfo.InvariantCulture)){
+                        var listadoProductosCsv = csvProductos.GetRecords<Producto>();
+
+                        List<Producto> productos = new List<Producto>();
+                        foreach(var item in listadoProductosCsv){
+                            productos.Add(new Producto{
+                                Id = item.Id,
+                                Nombre = item.Nombre,
+                                Precio = item.Precio,
+                                FechaCreacion = item.FechaCreacion,
+                                CategoriaId = item.CategoriaId,
+                                MarcaId = item.MarcaId,                                                               
+                            });
+                        }
+
+                        context.Productos.AddRange(productos);
+                        await context.SaveChangesAsync();
+                    }
+                }
+            }
+
+
         }
-            }
-            catch(Exception ex){
-                var logger = loggerFactory.CreateLogger<TiendaContextSeed>();
-                logger.LogError(ex.Message);
-            }
+        catch (Exception ex)
+        {
+             var logger = loggerFactory.CreateLogger<TiendaContextSeed>();
+             logger.LogError(ex.Message);
         }
     }
+}
